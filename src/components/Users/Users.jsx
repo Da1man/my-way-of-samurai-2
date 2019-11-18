8e263e157e2a3ab30c87b4489e0fcbaa23e0c84c
Da1man <chaos4ever@mail.ru>
2019-11-18 22:47:10 +0300

    lesson 54 done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,9 +5,8 @@ import userPhoto from "../../assets/images/user-512x512.png"
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-        alert('new')
+        componentDidMount() {
+
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
                 this.props.setUsers(response.data.items);
             });
